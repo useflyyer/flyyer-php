a27e88e1e4895ba733c1d9cabeeac2fc7b73ca5b
Franco Méndez <f@francomendez.com>
2021-04-12 15:56:18 -0400

    feat: add stable AI implementation with signatures

--- src/FlayyerAI.php
+++ src/FlayyerAI.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use \Firebase\JWT\JWT;
+
 final class FlayyerAI
 {
   /**
-   * Your project slug.
+   * Required. Your project slug.
    */
   public $project;
   /**
@@ -25,7 +27,7 @@ final class FlayyerAI
    */
   public $secret;
   /**
-   * Optional. `HMAC` or `JWT` depending on which signature strategy you want.
+   * Optional. Possible values are `HMAC` or `JWT` depending on which signature strategy you want.
    */
   public $strategy;
 
@@ -35,8 +37,8 @@ final class FlayyerAI
   public function __construct(
     $project,
     $path = '/',
-    $variables = null,
-    $meta = null,
+    $variables = [],
+    $meta = [],
     $secret = null,
     $strategy = null
   ) {
@@ -54,29 +56,84 @@ final class FlayyerAI
   public static function to_query($hash)
   {
     // TODO: add more tests and edge-cases.
-    return http_build_query($hash);
+    $aux = explode("&", http_build_query($hash));
+    sort($aux);
+    return implode("&", $aux);
+  }
+
+  /**
+   * Get current path with slash at start
+   */
+  public function path_safe()
+  {
+    return substr($this->path, 0, 1) === "/" ? $this->path : "/" . $this->path;
+  }
+
+  /**
+   * Get current params as hash (meta & variables)
+   */
+  public function params_hash($ignoreV)
+  {
+    $defaults = [
+      "__v" => $this->meta["v"] ?: round(microtime(true)),
+      "__id" => $this->meta["id"] ?: null,
+      "_w" => $this->meta["width"] ?: null,
+      "_h" => $this->meta["height"] ?: null,
+      "_res" => $this->meta["resolution"] ?: null,
+      "_ua" => $this->meta["agent"] ?: null,
+    ];
+    if ($ignoreV) {
+      unset($defaults["__v"]);
+    }
+    return array_merge($defaults, $this->variables);
   }
 
   /**
    * Get final querystring with added '__v' param to force crawlers to update the image.
    */
-  public function querystring()
+  public function querystring($ignoreV = false)
   {
-    $defaults = ['__v' => round(microtime(true))];
-    if (empty($this->variables)) {
-      return FlayyerAI::to_query($defaults);
+    return FlayyerAI::to_query(array_filter($this->params_hash($ignoreV)));
+  }
+
+  /**
+   * Signatures
+   */
+  public function sign()
+  {
+    if (is_null($this->strategy) and is_null($this->secret)) {
+      return '_';
+    }
+    if (is_null($this->secret)) throw new Exception('Got `strategy` but missing `secret`. You can find it in your project in Advanced settings.');
+    if (is_null($this->strategy)) throw new Exception('Got `secret` but missing `strategy`. Valid options are `HMAC` or `JWT`.');
+
+    $key = $this->secret;
+    $strategy_lowercased = strtolower($this->strategy);
+    if ($strategy_lowercased == "hmac") {
+      $data = "{$this->project}{$this->path_safe()}{$this->querystring(true)}";
+      $mac = hash_hmac('sha256', $data, $key);
+      return substr($mac, 0, 16);
+    } elseif ($strategy_lowercased == "jwt") {
+      $payload = array("params" => array_filter($this->params_hash(true)), "path" => $this->path_safe());
+      return JWT::encode($payload, $key);
     } else {
-      return FlayyerAI::to_query(array_merge($defaults, $this->variables));
+      throw new Exception('Invalid `strategy`. Valid options are `HMAC` or `JWT`.');
     }
   }
 
   /**
-   * Get final FLAYYER url. Use this as value (or content) of your <head> tags.
+   * Get final FLAYYER AI url. Use this as value (or content) of your <head> tags.
    */
   public function href()
   {
     if (empty($this->project)) throw new Exception('Missing \'project\' property');
-
-    return "https://flayyer.ai/v2/{$this->project}/_/_{$this->path}";
+    $signature = $this->sign();
+    $params = $this->querystring(false);
+    $version = $this->meta["v"] ?? round(microtime(true));
+    if (is_null($this->strategy) || strtolower($this->strategy) == "hmac") {
+      return "https://flayyer.ai/v2/{$this->project}/{$signature}/{$params}{$this->path_safe()}";
+    } else {
+      return "https://flayyer.ai/v2/{$this->project}/jwt-{$signature}?__v={$version}";
+    }
   }
 }
