8aa001cd3a890877c94aad82a796d515d17ec7c5
Franco Méndez <f@francomendez.com>
2021-08-19 18:34:48 -0400

    feat: Support Flyyer Render signatures

--- src/FlyyerRender.php
+++ src/FlyyerRender.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use \Firebase\JWT\JWT;
+
 final class FlyyerRender
 {
   /**
@@ -28,24 +30,42 @@ final class FlyyerRender
    * JS serializable variables.
    */
   public $variables;
+  /**
+   * Optional. `id` is suggested as your product/post identifier. {id, v, width, height, resolution, agent}
+   */
+  public $meta;
+  /**
+   * Optional. Your HMAC secret you can find on your dashboard under Advanced settings for signed requests.
+   */
+  public $secret;
+  /**
+   * Optional. Possible values are `HMAC` or `JWT` depending on which signature strategy you want.
+   */
+  public $strategy;
 
   /**
-   * Construct a FLYYER helper object.
+   * Construct a FLYYER RENDER helper object.
    */
   public function __construct(
     $tenant,
     $deck,
     $template,
     $version = null,
-    $extension = 'jpeg',
-    $variables = []
+    $extension = null,
+    $variables = [],
+    $meta = [],
+    $secret = null,
+    $strategy = null
   ) {
     $this->tenant = $tenant;
     $this->deck = $deck;
     $this->template = $template;
     $this->version = $version;
     $this->extension = $extension;
-    $this->variables = $variables;
+    $this->variables = $variables ?: [];
+    $this->meta = $meta;
+    $this->secret = $secret;
+    $this->strategy = $strategy;
   }
 
   /**
@@ -62,11 +82,35 @@ final class FlyyerRender
    */
   public function querystring()
   {
-    $defaults = ['__v' => round(microtime(true))];
-    if (empty($this->variables)) {
-      return FlyyerRender::to_query($defaults);
+    $default_v = ['__v' => round(microtime(true))];
+    $defaults = [
+      '__id' => $this->meta['id'] ?: null,
+      '_w' => $this->meta['width'] ?: null,
+      '_h' => $this->meta['height'] ?: null,
+      '_res' => $this->meta['resolution'] ?: null,
+      '_ua' => $this->meta['agent'] ?: null
+    ];
+    if ($this->strategy && $this->secret) {
+      $key = $this->secret;
+      if (strtolower($this->strategy) === 'hmac') {
+        $default_query = FlyyerRender::to_query($defaults);
+        $data = implode('#', [$this->deck, $this->template, $this->version ?: "", $this->extension ?: "", $default_query]);
+        $__hmac = substr(hash_hmac('sha256', $data, $key), 0, 16);
+        return FlyyerRender::to_query(array_merge($defaults, $default_v, $this->variables, ['__hmac' => $__hmac]));
+      } elseif (strtolower($this->strategy) === 'jwt') {
+        $jwt_defaults = [
+          'i' => $this->meta['id'] ?: null,
+          'w' => $this->meta['width'] ?: null,
+          'h' => $this->meta['height'] ?: null,
+          'r' => $this->meta['resolution'] ?: null,
+          'u' => $this->meta['agent'] ?: null
+        ];
+        $payload = array_merge(array('d' => $this->deck, 't' => $this->template, 'v' => $this->version, 'e' => $this->extension, 'var' => $this->variables), $jwt_defaults);
+        $__jwt = JWT::encode($payload, $key);
+        return FlyyerRender::to_query(array_merge([ '__jwt' => $__jwt ], $default_v));
+      }
     } else {
-      return FlyyerRender::to_query(array_merge($defaults, $this->variables));
+      return FlyyerRender::to_query(array_merge($default_v, $this->variables));
     }
   }
 
@@ -78,11 +122,20 @@ final class FlyyerRender
     if (empty($this->tenant)) throw new Exception('Missing \'tenant\' property');
     if (empty($this->deck)) throw new Exception('Missing \'deck\' property');
     if (empty($this->template)) throw new Exception('Missing \'template\' property');
+    if ($this->secret && empty($this->strategy)) throw new Exception('Got `secret` but missing `strategy`.  Valid options are `HMAC` or `JWT`.');
+    if ($this->strategy && empty($this->secret)) throw new Exception('Got `strategy` but missing `secret`. You can find it in your project in Advanced settings.');
+    if ($this->strategy && strtolower($this->strategy) != "jwt" && strtolower($this->strategy) != "hmac") throw new Exception('Invalid signing `strategy`. Valid options are `HMAC` or `JWT`.');
 
-    $query = $this->querystring();
-    if ($this->version) {
-      return "https://cdn.flyyer.io/render/v2/{$this->tenant}/{$this->deck}/{$this->template}.{$this->version}.{$this->extension}?{$query}";
+    $base_href = "https://cdn.flyyer.io/r/v2/{$this->tenant}";
+
+    if ($this->strategy && strtolower($this->strategy) === "jwt") {
+      return "{$base_href}?{$this->querystring()}";
     }
-    return "https://cdn.flyyer.io/render/v2/{$this->tenant}/{$this->deck}/{$this->template}.{$this->extension}?{$query}";
+
+    $final_href = "{$base_href}/{$this->deck}/{$this->template}";
+    if ($this->version) $final_href .= ".{$this->version}";
+    if ($this->extension) $final_href .= ".{$this->extension}";
+    $final_href .= "?{$this->querystring()}";
+    return $final_href;
   }
 }
